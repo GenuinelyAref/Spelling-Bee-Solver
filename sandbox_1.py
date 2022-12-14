afec996271782479e60ee32737df0bc9a4346da1
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-12-14 23:59:53 +1300

    experiment with creating new folder
    use os library

--- sandbox_1.py
+++ sandbox_1.py
@@ -1,5 +1,42 @@
-print("\n\n\033[0;37;41m \033[0m Red label for error messages\n")
+"""print("\n\n\033[0;37;41m \033[0m Red label for error messages\n")
 print("\033[0;37;43m \033[0m Yellow/Orange label for information messages (no action required)\n")
 print("\033[0;37;44m \033[0m Blue label for prompts (action required)\n")
 print("\033[0;37;45m \033[0m Purple label for new words found while computing\n")
-print("\033[0;37;46m \033[0m Teal/Aqua label for output messages; results, progress updates and thank-you message\n")
+print("\033[0;37;46m \033[0m Teal/Aqua label for output messages; results, progress updates and thank-you message\n")"""
+
+# importing os module
+import os
+
+# Directory
+directory = "GeeksforGeeks"
+
+# Parent Directory path
+parent_dir = "D:/Pycharm projects/"
+
+# Path
+path = os.path.join(parent_dir, directory)
+
+# Create the directory
+# 'GeeksForGeeks' in
+# '/home / User / Documents'
+os.mkdir(path)
+print("Directory '% s' created" % directory)
+
+# Directory
+directory = "Geeks"
+
+# Parent Directory path
+parent_dir = "D:/Pycharm projects"
+
+# mode
+mode = 0o666
+
+# Path
+path = os.path.join(parent_dir, directory)
+
+# Create the directory
+# 'GeeksForGeeks' in
+# '/home / User / Documents'
+# with mode 0o666
+os.mkdir(path, mode)
+print("Directory '% s' created" % directory)
